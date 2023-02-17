16ae07695c4aed7992d1c55a6db239fb80547121
Harley <yu.h.lin1991@gmail.com>
2023-02-18 00:04:06 +0800

    finished project page responsive styles setting

---
+++ components/Contact.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+export class Contact extends PureComponent {
+  render() {
+    return (
+      <div className="flex flex-col h-screen text-center md:text-left  max-w-7xl justify-evenly items-center mx-auto">
+        Contact
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,7 +7,6 @@ import projectData from "./../data/projects.json";
 
 import PreviewIcon from "@mui/icons-material/Preview";
 import GitHubIcon from "@mui/icons-material/GitHub";
-import Link from "@mui/material/Link";
 
 type Props = {};
 
@@ -46,54 +45,60 @@ const Projects = (props: Props) => {
           duration: 2,
         }}
       >
-        <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory md:p-6 scrollbar- scrollbar-thin scrollbar-track-[#E5E9F0]/25 scrollbar-thumb-[#88C0D0] scrollbar-thumb-rounded">
-          <Masonry columns={3} spacing={2}>
+        <div className="w-full flex flex-row overflow-x-scroll snap-x snap-mandatory md:p-4 scrollbar- scrollbar-thin scrollbar-track-[#E5E9F0]/25 scrollbar-thumb-[#88C0D0] scrollbar-thumb-rounded">
+          <Masonry
+            className="xs: px-2"
+            columns={{ xs: 2, sm: 3, md: 3 }}
+            spacing={{ xs: 1, sm: 2, md: 4 }}
+          >
             {projectData.map((element, index) => {
               return (
-                <div key={index}>
+                <div key={index} className="flex flex-col justify-center gap-1">
+                  <div className="cursor-pointer flex flex-col flex-wrap gap-2 justify-evenly items-center text-sm text-white text-medium">
+                    {element.name}
+                  </div>
                   <div className="relative overflow-hidden bg-no-repeat bg-cover w-full">
                     <img
-                      className="rounded hover:scale-110 transition duration-300"
+                      className="rounded-lg hover:scale-110 transition duration-300"
                       src={element.image}
                     />
-                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-90 transition duration-300 ease-in-out bg-slate-800">
-                      <div className="w-full h-full flex flex-col flex-wrap gap-3 p-4 justify-evenly items-center text-white text-sm md:text-xs">
-                        <div className="font-medium text-white text-lg md:text-base">
-                          {element.name}
-                        </div>
-                        <div className="font-normal">{element.description}</div>
-                        <div className="flex flex-rol flex-wrap gap-3">
+                    <div className="absolute rounded top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 text-white text-xs hover:opacity-90 transition duration-300 ease-in-out bg-slate-800">
+                      <div className="w-full h-full flex flex-wrap flex-col justify-evenly items-center gap-2 p-4">
+                        <div className="font-thin">{element.description}</div>
+                        <div className="flex flex-rol flex-wrap gap-3 text-xs text-white">
                           {element.skills?.map((skill, index) => {
                             return (
                               <span
                                 className="flex flex-rol gap-1 items-center pr-1"
                                 key={index}
                               >
-                                <i className={`${skill.icon} text-base`}></i>
+                                <i
+                                  className={`${skill.icon} text-base md:text-sm`}
+                                ></i>
                                 {skill.name}
                               </span>
                             );
                           })}
                         </div>
-                        <div className="flex items-center gap-2 text-sm">
-                          Links:
-                          {element.github ? (
-                            <GitHubIcon
-                              className="cursor-pointer w-5"
-                              onClick={() => {
-                                window.open(element.demo, "_blank");
-                              }}
-                            />
-                          ) : null}
-                          <PreviewIcon
-                            className="cursor-pointer"
-                            onClick={() => {
-                              window.open(element.demo, "_blank");
-                            }}
-                          />
-                        </div>
                       </div>
                     </div>
+                  </div>
+                  <div className="flex justify-center items-center gap-2 text-xs">
+                    Links:
+                    {element.github ? (
+                      <GitHubIcon
+                        className="cursor-pointer w-5"
+                        onClick={() => {
+                          window.open(element.demo, "_blank");
+                        }}
+                      />
+                    ) : null}
+                    <PreviewIcon
+                      className="cursor-pointer"
+                      onClick={() => {
+                        window.open(element.demo, "_blank");
+                      }}
+                    />
                   </div>
                 </div>
               );
